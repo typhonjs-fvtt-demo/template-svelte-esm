729240f2b238d66e0e26cad7542cf44b776816cc
typhonrt <support@typhonjs.io>
2024-10-22 20:13:49 -0700

    Update config.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,12 @@
 /* eslint-env node */
-import { svelte }    from '@sveltejs/vite-plugin-svelte';
-import resolve       from '@rollup/plugin-node-resolve'; // This resolves NPM modules from node_modules.
-import preprocess    from 'svelte-preprocess';
+import resolve                from '@rollup/plugin-node-resolve'; // This resolves NPM modules from node_modules.
+import { svelte }             from '@sveltejs/vite-plugin-svelte';
+
 import {
    postcssConfig,
-   terserConfig }    from '@typhonjs-fvtt/runtime/rollup';
+   terserConfig }             from '@typhonjs-fvtt/runtime/rollup';
+
+import { sveltePreprocess }   from 'svelte-preprocess';
 
 // ATTENTION!
 // Please modify the below variables: s_PACKAGE_ID and s_SVELTE_HASH_ID appropriately.
@@ -102,7 +104,7 @@ export default () =>
                // TRL components and makes it easier to review styles in the browser debugger.
                cssHash: ({ hash, css }) => `svelte-${s_SVELTE_HASH_ID}-${hash(css)}`
             },
-            preprocess: preprocess()
+            preprocess: sveltePreprocess()
          }),
 
          resolve(s_RESOLVE_CONFIG)  // Necessary when bundling npm-linked packages.
